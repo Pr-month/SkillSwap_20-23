2c6b8ef8839aa98ac9b7eb7df8a7e96f24ed3159
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-07-17 00:14:49 +0300

    Revert " feat: добавлен модуль конфигурации и файл .env.example"
    This reverts commit 8ac469d223e64702dcd6c5fa1af219ac71cbce11.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './data-source';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'production' ? '.env.prod' : '.env',
-    }),
     TypeOrmModule.forRoot(AppDataSource.options),
   ],
   

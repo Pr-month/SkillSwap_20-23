8ac469d223e64702dcd6c5fa1af219ac71cbce11
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-07-16 21:08:13 +0300

    feat: добавлен модуль конфигурации и файл .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './data-source';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env.prod' : '.env',
+    }),
     TypeOrmModule.forRoot(AppDataSource.options),
   ],
   

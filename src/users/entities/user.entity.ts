b0857a28c56d08c39958f9f7858183ef3a55309d
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-07-17 11:20:31 +0300

    feat/user.enitity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,46 @@
-export class User {}
+import { IsEmail } from "class-validator";
+import { Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
+
+type Gender = 'male' | 'female';
+type Role = 'USER' | 'ADMIN';
+
+@Entity()
+export class User {
+    @PrimaryColumn({ type: 'uuid' })
+    id: string;
+    @Column({type: 'text'})
+    name: string;
+    @Column({ type: 'text'})
+    @IsEmail()
+    email: string;
+    @Column({ type: 'text' })
+    password: string;
+    @Column({ type: 'int' })
+    age: number;
+    @Column({type: 'text'})
+    city: string;
+    @Column({
+        type: 'enum',
+        enum: ['male', 'female'],
+    })
+    gender: Gender;
+    @Column({type: 'text'})
+    avatar: string;
+    @Column({
+        type: 'enum',
+        enum: ['ADMIN', 'USER']
+    })
+    role: Role;
+    @Column({
+        type: 'varchar',
+        length: 255,
+        unique: true
+    })
+    refreshToken: string
+    // @OneToMany(() => Skill, (skill) => skill.id)
+    // skills: Skill
+    // @OneToMany(() => Skill, (skill) => skill.id)
+    // wantToLearn: Skill
+    // @OneToMany(() => Skill, (skill) => skill.id)
+    // favoriteSkills: Skill
+}

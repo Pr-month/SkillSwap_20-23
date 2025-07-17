7f3fbcff0ed0167706ffa14e30bf6f4a644a52d7
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-17 21:36:31 +0300

    fix (main.ts and data-source.ts) : add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 // ЗАТЫЧКА ЛИНТИНГА
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();

5bc6782b6309c5bbca5dde6f705cdb0b866605f2
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-07-17 13:04:19 +0300

    feeat: Подключить в main.ts пайпу валидации

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // удаляет все свойства, которых нет в DTO 
+      forbidNonWhitelisted: true, // выбрасывает ошибку, если есть лишние поля
+      transform: true, // автоматически преобразует payload к типу DTO
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 // ЗАТЫЧКА ЛИНТИНГА

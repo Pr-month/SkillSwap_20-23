991f5b479221f30fb6e93acae1323c2c35855843
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2025-07-17 15:59:05 +0300

    fix ( data-source.ts ) : изменение параметров работы с БД

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,5 +7,6 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: false,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true,
 });

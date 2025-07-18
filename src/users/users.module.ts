12221918c7a9c930754adbb22d8c5a04f3f6eeaa
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-19 00:15:18 +0300

    fix (users.module.ts) : обновление конфигурации модуля - пользователь
    format (users.module.ts) : форматирование файла

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { Users } from './entities/users.entity';
 import { UsersController } from './users.controller';
 
 @Module({
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([Users])],
   providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}

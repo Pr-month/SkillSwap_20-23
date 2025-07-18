6ea4a2b7826bd4a20e456d6bd113b1c0d58a64f9
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-19 00:14:27 +0300

    feat (create-user.dto.ts) : конфигурация пользовательского представления для операции создания

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { Gender } from 'src/common/gender.enum';
+import { Role } from 'src/common/role.enum';
+
+export class CreateUserDto {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  age: number;
+  city: string;
+  gender: Gender;
+  avatar: string;
+  role: Role;
+  refreshToken: string;
+}

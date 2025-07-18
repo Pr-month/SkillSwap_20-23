014cf0f742d2eb5e33292be88e8858c013f42afd
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-07-18 21:51:13 +0300

    Update accessToken.strategies.ts

--- src/auth/strategies/accessToken.strategies.ts
+++ src/auth/strategies/accessToken.strategies.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
-import { ExtractJwt, Strategy,StrategyOptions } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../auth.types';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(configService: ConfigService) {
-    
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-    const jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-
     super({
-      jwtFromRequest,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey:
         configService.get<string>('JWT_SECRET') || 'defaultSecretKey',
       ignoreExpiration: false,

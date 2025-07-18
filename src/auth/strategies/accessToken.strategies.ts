857bfd229cc08196607b8af7809318a8bf53792d
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-07-18 21:50:07 +0300

    Revert "Update accessToken.strategies.ts"
    This reverts commit 97091912f02bf41201c5af9173a8498ef61205f5.

--- src/auth/strategies/accessToken.strategies.ts
+++ src/auth/strategies/accessToken.strategies.ts
@@ -1,25 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy,StrategyOptions } from 'passport-jwt';
 import { JwtPayload } from '../auth.types';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(configService: ConfigService) {
-    // Явно типизируем вызов ExtractJwt
-    const jwtExtractor = ExtractJwt.fromAuthHeaderAsBearerToken();
-    const jwtFromRequest = (req: any) => {
-      try {
-        return jwtExtractor(req);
-      } catch (err) {
-        return null;
-      }
-    };
+    
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    const jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 
     super({
       jwtFromRequest,
-      secretOrKey: configService.get<string>('JWT_SECRET') || 'defaultSecretKey',
+      secretOrKey:
+        configService.get<string>('JWT_SECRET') || 'defaultSecretKey',
       ignoreExpiration: false,
     });
   }

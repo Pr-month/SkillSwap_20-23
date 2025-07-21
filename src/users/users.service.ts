16fbe35ce403d9de2a873dd4e82bcbfdf8bcd7c7
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-21 19:16:03 +0300

    feat (users.service.ts) : изменение метода findId и создание updateUserById

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Users } from './entities/users.entity';
-import { Repository } from 'typeorm';
+import { User } from './entities/users.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(Users)
-    private usersRepository: Repository<Users>,
+    @InjectRepository(User)
+    private usersRepository: UserRepository,
   ) {}
 
-  async findAll(): Promise<Users[]> {
+  async findAll(): Promise<User[]> {
     return await this.usersRepository.find();
   }
 
-  async findId(id: number) {
-    return await this.usersRepository.findOneOrFail({ where: { id } });
+  async findId(id: string) {
+    const user = await this.usersRepository.findOneOrFail({ where: { id } });
+
+    if (!user) {
+      throw new UnauthorizedException('Пользователь не найден');
+    }
+    const { password, refreshtoken, ...returnValues } = user;
+    return returnValues;
+  }
+
+  async updateUserById(id: string, updateUserDto: UpdateUserDto) {
+    // Поскольку пароль и рефереш токен надо выкинуть оставлю здесь эту линию
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, refreshtoken, ...updatedUser } =
+      await this.usersRepository.updateUserById(id, updateUserDto);
+    return updatedUser;
   }
 }

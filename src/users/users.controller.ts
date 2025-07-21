3d325086c5201b0c833f8774a3bf990df953468e
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-21 19:14:18 +0300

    feat (users.controller.ts) : добавление гарды на контроллер /me и добавление метода path

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Get,
+  Body,
+  Patch,
+  BadRequestException,
+  UseGuards,
+  Param,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AccessTokenGuard } from 'src/auth/guards/accessToken.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { RequestWithGuard, UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,13 +22,32 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Get('/me')
-  findMe() {
-    return this.usersService.findId(1);
+  @Get('me')
+  @UseGuards(AuthGuard('jwt'))
+  async getMe(@Request() req: RequestWithGuard) {
+    const currentUser = await this.usersService.findId(req.user.userId);
+    return currentUser;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  findOne(@Param('id') id: string) {
     return this.usersService.findId(id);
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Patch('me')
+  async updateMe(
+    @Request() req: RequestWithGuard,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    try {
+      const response = await this.usersService.updateUserById(
+        req.user.userId,
+        updateUserDto,
+      );
+      return response;
+    } catch {
+      throw new BadRequestException('Ошибка при обновлении пользователя');
+    }
   }
 }

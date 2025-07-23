c401fa423b717bc30d0a1368364d88cb33d600a4
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-23 21:07:04 +0300

    fix : connect to postgres

--- docker-entrypoint-initdb.d/01_INITIALIZE_DB.sh
+++ docker-entrypoint-initdb.d/01_INITIALIZE_DB.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_DB" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_NAME" ]; then
+  echo "Ошибка: не все переменные окружения установлены"
+  exit 1
+fi
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER "$DB_USER" PASSWORD '$DB_PASSWORD';
     CREATE DATABASE "$DB_NAME";
-    GRANT ALL PRIVILEGES ON DATABASE "$DB_NAME" TO "$DB_USER"
+    GRANT ALL PRIVILEGES ON DATABASE "$DB_NAME" TO "$DB_USER";
 EOSQL

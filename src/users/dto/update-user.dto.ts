784f4e8a65c648b32eb75862a5942bb64e58df9e
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-24 01:51:06 +0300

    fix (update-user.dto.ts) : изменение DTO представления

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,13 +1,52 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-import { Gender } from '../../common/gender.enum';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsEnum,
+  IsInt,
+  Min,
+  Max,
+} from 'class-validator';
+import { Gender } from 'src/common/gender.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
-  age: number;
-  city: string;
-  about: string;
-  gender: Gender;
-  avatar: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  about?: string | null; // Разрешаем null;
+
+  @IsOptional()
+  @IsInt()
+  @Min(16)
+  @Max(100)
+  age?: number | null; // Разрешаем null
+  
+  @IsOptional()
+  @IsString()
+  city?: string | null;;
+
+  @IsOptional()
+  @IsEnum(Gender)
+  gender?: Gender;
+
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+
+  @IsString()
+  refreshToken: string
 }

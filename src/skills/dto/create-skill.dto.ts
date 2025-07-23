fedcaf35dfc930948b74970b4ac094e075a8b32f
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-23 23:18:53 +0300

    feat (create-skill.dto.ts) : настройка полей DTO и их валидация

--- src/skills/dto/create-skill.dto.ts
+++ src/skills/dto/create-skill.dto.ts
@@ -1,1 +1,26 @@
-export class CreateSkillDto {}
+import { IsArray, IsNotEmpty, IsString, IsUUID } from "class-validator";
+
+export class CreateSkillDto {
+    
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @IsNotEmpty()
+    @IsArray()
+    images?: string[];
+
+    @IsString()
+    @IsNotEmpty()
+    @IsUUID()
+    ownerId: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsUUID()
+    categoryId: string;
+}

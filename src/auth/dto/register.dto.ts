68f079af1ed322a52fdb86c12ce8dcfc706ff53b
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2025-07-24 01:51:19 +0300

    fix (register.dto.ts) : изменение DTO представления

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,19 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
 import {
   IsEmail,
   IsNotEmpty,
   IsString,
   MinLength,
-  IsOptional,
-  IsEnum,
-  IsInt,
-  Min,
-  Max,
 } from 'class-validator';
-import { Gender } from 'src/common/gender.enum';
-import { User } from 'src/users/entities/user.entity';
 
-export class RegisterDto extends PartialType(User) {
+export class RegisterDto {
   @IsString()
   @IsNotEmpty()
   name: string;
@@ -26,26 +18,4 @@ export class RegisterDto extends PartialType(User) {
   @IsNotEmpty()
   @MinLength(6)
   password: string;
-
-  @IsString()
-  @IsNotEmpty()
-  about: string;
-
-  @IsOptional()
-  @IsInt()
-  @Min(16)
-  @Max(100)
-  age?: number;
-
-  @IsOptional()
-  @IsString()
-  city?: string;
-
-  @IsOptional()
-  @IsEnum(Gender)
-  gender?: Gender;
-
-  @IsOptional()
-  @IsString()
-  avatar?: string;
 }

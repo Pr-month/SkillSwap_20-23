1c0daa7eb20effe0265f178e25198dc0ebcce7e3
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-07-23 14:17:04 +0300

    feat:  Настройка логирования через winston

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,25 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { WinstonLogger } from './logger/winston-logger';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  //const app = await NestFactory.create(AppModule);
+
+  // Создание приложения с буферизацией логов
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true, // Важно для корректной работы кастомного логгера
+  });
+
+   // Получаем экземпляры сервисов
+  //const configService = app.get(ConfigService);
+  const logger = app.get(WinstonLogger);
+
+  // Устанавливаем глобальный логгер
+  app.useLogger(logger);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // удаляет все свойства, которых нет в DTO
@@ -13,6 +28,9 @@ async function bootstrap() {
       transform: true, // автоматически преобразует payload к типу DTO
     }),
   );
+
+  logger.log(`Application started on port ${process.env.PORT || 3000}`, 'Bootstrap'); //логирование порта приложения
+  logger.log(`Environment: ${process.env.NODE_ENV || 'development'}`, 'Bootstrap'); //логирование окружения
   await app.listen(process.env.PORT ?? 3000);
 }
 

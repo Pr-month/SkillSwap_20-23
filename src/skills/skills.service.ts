b413de17a3f82199a90536b2628e7b94f6c6639d
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2025-07-24 15:52:20 +0300

    fix (skills.service.ts) : оптимизация методов update findOne \ findAll \ create

--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -17,23 +17,50 @@ export class SkillsService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
-  async create(createSkillDto: CreateSkillDto): Promise<Skill> {
-    try {
-      const newSkill = this.skillRepository.create(createSkillDto) as Skill;
-      return await this.skillRepository.save(newSkill);
-    } catch (error) {
-      throw new Error('Ошибка при создании навыка: ' + error.message);
-    }
-  }
-
   async findAll(): Promise<Skill[]> {
     return await this.skillRepository.find();
   }
 
-  findOne(id: string): Promise<Skill> {
-    const skill = this.skillRepository.findOneOrFail({ where: { id } });
-    return skill
+  async findOne(skillId: string): Promise<Skill> {
+    return await this.skillRepository.findOneOrFail({ where: { id: skillId } });
   }
+
+  async create(userId: string, createSkillDto: CreateSkillDto): Promise<Skill> {
+    try {
+      const user = await this.userRepository.findOneOrFail({
+        where: { id: userId },
+      });
+
+      const newSkill = this.skillRepository.create({
+        ...createSkillDto,
+        owner: { id: user.id },
+      });
+
+      return await this.skillRepository.save(newSkill);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to update user',
+        String(error),
+      );
+    }
+  }
+
+  async update(
+    userId: string,
+    skillId: string,
+    updateSkillDto: UpdateSkillDto,
+  ) {
+    try {
+      await this.userRepository.findOneOrFail({
+        where: { id: userId },
+      });
+
+      const skill = await this.skillRepository.findOneOrFail({
+        where: { id: skillId },
+      });
+
+      const mergedSkill = this.skillRepository.merge(skill, updateSkillDto);
+      const savedSkill = await this.skillRepository.save(mergedSkill);
 
       return savedSkill;
     } catch (error) {

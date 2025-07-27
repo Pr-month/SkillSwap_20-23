6afcb4544207dc2ff070f238673e4b27c5029d1c
DKSolo <dbm95mail@gmail.com>
2025-07-27 07:22:04 +0500

    feat: update-category.dto with class-validator

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -2,10 +2,35 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
 import { Category } from '../entities/category.entity';
 import { Skill } from 'src/skills/entities/skill.entity';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
-  name: string;
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  name?: string;
+
+  @IsOptional()
+  @IsUUID()
   parent?: Category;
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Category)
   children?: Category[];
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Skill)
   skills?: Skill[];
 }

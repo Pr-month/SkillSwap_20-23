258877e5c66c19c3c99611e8082048bbcc168bbc
SinitsaBogdan <sinitsa.bogdan.1997@yandex.ru>
2025-08-03 13:42:42 +0300

    find (requests.service.ts) : реализация метода сервиса обновления запроса

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,35 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
 import { Request } from './entities/request.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { Role } from 'src/common/types';
 
 @Injectable()
 export class RequestsService {
-  constructor(
-    @InjectRepository(Request) private userRepository: Repository<Request>,
-  ) {}
+    constructor(
+        @InjectRepository(Request) private requestRepository: Repository<Request>,
+        private readonly userService: UsersService,
+    ) { }
 
-  create(createRequestDto: CreateRequestDto) {
-    console.log(createRequestDto);
-    return 'This action adds a new request';
-  }
+    create(createRequestDto: CreateRequestDto) {
+        console.log(createRequestDto);
+        return 'This action adds a new request';
+    }
 
-  findAll() {
-    return `This action returns all requests`;
-  }
+    findAll() {
+        return `This action returns all requests`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} request`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} request`;
+    }
 
-  update(id: number, updateRequestDto: UpdateRequestDto) {
-    console.log(updateRequestDto);
-    return `This action updates a #${id} request`;
-  }
+    async update(requestId: string, updateRequestDto: UpdateRequestDto, userId: string) {
+        const request = await this.requestRepository.findOneOrFail({
+            where: {
+                id: requestId,
+            },
+        });
 
-  remove(id: number) {
-    return `This action removes a #${id} request`;
-  }
+        const user = await this.userService.findUserById(userId);
+
+        if (user.id !== request.sender.id || user.role !== Role.ADMIN)
+            throw new ForbiddenException('Недостаточно прав');
+
+        return this.requestRepository.save({
+            ...request,
+            ...updateRequestDto,
+        });
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} request`;
+    }
 }

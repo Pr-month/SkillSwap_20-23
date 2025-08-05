a132cb126a3b2422577cacbd315ebeebf2f08c1e
DKSolo <dbm95mail@gmail.com>
2025-08-05 16:29:47 +0500

    FIX: imageFileFilter error fixed by moving const imageFileFilter

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -11,22 +11,6 @@ import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Request } from 'express';
 
-@Controller('files')
-export class UploadController {
-  constructor(private readonly uploadService: UploadService) {}
-
-  @Post()
-  @UseInterceptors(
-    FileInterceptor('file', {
-      fileFilter: imageFileFilter,
-    }),
-  )
-  uploadFile(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
-    const fileUrl = `${req.protocol}://${req.get('host')}`;
-    return this.uploadService.handleFileUpload(fileUrl, file);
-  }
-}
-
 // Функция фильтрации файлов
 // TODO: может нужно будет вынести в отдельный файл.
 const imageFileFilter = (
@@ -48,3 +32,19 @@ const imageFileFilter = (
 
   callback(null, true);
 };
+
+@Controller('files')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: imageFileFilter,
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
+    const fileUrl = `${req.protocol}://${req.get('host')}`;
+    return this.uploadService.handleFileUpload(fileUrl, file);
+  }
+}

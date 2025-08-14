953596f60038602ce7fe95b41265c6c2d06f93e0
Roland <machodin@yandex.ru>
2025-08-15 00:05:16 +0300

    resolve errors after merge

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,7 +5,7 @@ import { SkillsModule } from '../skills/skills.module';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { SkillsModule } from '../skills/skills.module';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]), // Регистрация репозитория User

7c8879848a58bfeeb36c5f2db52fcd6816d2501b
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2025-08-15 13:41:28 +0300

    fix (create-category.dto.ts) : Добавление валидации поля name

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,8 +1,17 @@
-import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
+  @MaxLength(100, {
+    message: 'Длина названия категории не может превышать 100 символов',
+  })
   name: string;
 
   @IsOptional()

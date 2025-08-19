f030a3a8675c0584b8cb90ba0b38a1199580efe1
EkatrinaAK <137857957+EkatrinaAK@users.noreply.github.com>
2025-08-19 20:35:14 +0700

    feat: swagger users

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -12,54 +12,100 @@ import {
 } from 'class-validator';
 import { Gender } from '../../common/gender.enum';
 import { Skill } from '../../skills/entities/skill.entity';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional  } from '@nestjs/swagger';
 import { UUID } from 'typeorm/driver/mongodb/bson.typings';
 
 export class UpdateUserDto {
+  @ApiPropertyOptional({
+    description: 'Имя пользователя',
+    example: 'Алексей',
+  })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   name?: string;
 
+  @ApiPropertyOptional({
+    description: 'Email пользователя',
+    example: 'alex@example.com',
+  })
   @IsOptional()
   @IsEmail()
   @IsNotEmpty()
   email?: string;
 
+  @ApiPropertyOptional({
+    description: 'О себе',
+    example: 'Люблю программирование и спорт',
+    nullable: true,
+  })
   @IsOptional()
   @IsString()
   about?: string | null;
 
+  
+  @ApiPropertyOptional({
+    description: 'Дата рождения',
+    example: '1990-01-01',
+    nullable: true,
+  })
   @IsOptional()
   @IsDate()
   birthDate?: Date | null; // Разрешаем null
 
+  @ApiPropertyOptional({
+    description: 'Город',
+    example: 'Москва',
+    nullable: true,
+  })
   @IsOptional()
   @IsString()
   city?: string | null;
 
+  @ApiPropertyOptional({
+    description: 'Пол',
+    enum: Gender,
+    example: Gender.MALE,
+    nullable: true,
+  })
   @IsOptional()
   @IsEnum(Gender)
   gender?: Gender;
 
+  @ApiPropertyOptional({
+    description: 'URL аватара',
+    example: 'https://example.com/avatar.jpg',
+    nullable: true,
+  })
   @IsOptional()
   @IsString()
   avatar?: string;
 
+  @ApiPropertyOptional({
+    description: 'Refresh token',
+    example: 'refresh-token-string',
+    nullable: true,
+  })
   @IsOptional()
   @IsString()
   refreshToken: string;
 
+  @ApiPropertyOptional({
+    description: 'Любимые навыки',
+    type: [Skill],
+    nullable: true,
+  })
   @IsOptional()
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => Skill)
   favoriteSkills?: Skill[];
 
-  @ApiProperty({
+   @ApiPropertyOptional({
     description: 'Категории навыков, которые пользователь хочет изучить',
-    type: [UUID],
-    required: false,
+    type: [String],
+    example: ['123e4567-e89b-12d3-a456-426614174000'],
+    nullable: true,
   })
   @IsOptional()
   @IsArray()

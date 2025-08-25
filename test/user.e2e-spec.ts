2b2790373efff40ded80c119ad90cf171ca655f5
DKSolo <dbm95mail@gmail.com>
2025-08-25 18:23:31 +0500

    fix: users.e2e now works

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,15 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import * as bcrypt from 'bcrypt';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
-import {
-  AdminUsersData,
-  AdminUsersPassword,
-  TestUserPassword,
-  TestUsersData,
-} from '../src/scripts/users.data';
+import { TestUserPassword, TestUsersData } from '../src/scripts/users.data';
 import { AllExceptionFilter } from '../src/common/all-exception.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppDataSource } from '../src/config/data-source';
@@ -17,7 +11,6 @@ import { Repository } from 'typeorm';
 import { User } from '../src/users/entities/user.entity';
 import { Category } from '../src/categories/entities/category.entity';
 import { Role } from '../src/common/types';
-import { Categories } from '../src/scripts/categories.data';
 import { Skill } from '../src/skills/entities/skill.entity';
 
 // Making DTOs
@@ -67,13 +60,13 @@ describe('User module (e2e)', () => {
   let categoryRepo: Repository<Category>;
   let skillRepo: Repository<Skill>;
 
-  let testUsers: User[];
   let userPassword: string;
+  let someUserEntity: User;
   let someID: string;
-  let someName: string;
   let someEmail: string;
   let someUser: SomeUserDTO;
   let someSkillID: string;
+  let someSkillEntity: Skill;
   let someCategoryID: string;
   let jwtToken: string;
   const forgedJwtToken =
@@ -101,106 +94,80 @@ describe('User module (e2e)', () => {
     userRepo = AppDataSource.getRepository(User);
     categoryRepo = AppDataSource.getRepository(Category);
     skillRepo = AppDataSource.getRepository(Skill);
-
-    try {
-      const existingAdmin = await userRepo.findOne({
-        where: { email: AdminUsersData.email, role: Role.ADMIN },
-      });
-      if (existingAdmin && existingAdmin.email === AdminUsersData.email) {
-        console.log('Администратор уже существует');
-        return;
-      }
-
-      const admin = await userRepo.save(
-        userRepo.create({
-          ...AdminUsersData,
-          password: await bcrypt.hash(AdminUsersPassword, 10),
-        }),
-      );
-
-      await userRepo.save(admin);
-
-      // проверяем, есть ли уже данные в таблице
-      const existing = await categoryRepo.count();
-      if (existing > 0) {
-        await AppDataSource.destroy();
-        return;
-      }
-
-      await Promise.all(
-        Categories.map(async (categoryData) => {
-          const parentCategory = await categoryRepo.save(
-            categoryRepo.create({ name: categoryData.name }),
-          );
-
-          await Promise.all(
-            categoryData.children.map((childName) =>
-              categoryRepo.save(
-                categoryRepo.create({
-                  name: childName,
-                  parent: parentCategory,
-                }),
-              ),
-            ),
-          );
-        }),
-      );
-
-      const createdCategories = await categoryRepo.find();
-      const someCategory = createdCategories[0];
-      someCategoryID = createdCategories[0].id;
-
-      userPassword = TestUserPassword;
-      const userPasswordEncrypted = await bcrypt.hash(userPassword, 10);
-
-      for (const user of TestUsersData) {
-        const thisUser = await userRepo.findOne({
-          where: { email: user.email },
-        });
-
-        if (thisUser && thisUser.email === user.email) {
-          console.log(`Пользователь уже существует! ${thisUser.email}`);
-          return;
-        }
-
-        await userRepo.save(
-          userRepo.create({
-            ...user,
-            password: userPasswordEncrypted,
-            wantToLearn: [someCategory],
-          }),
-        );
-      }
-
-      //Creating skills to every user
-      testUsers = await userRepo.findBy({
-        role: Role.USER,
-      });
-      for (const user of testUsers) {
-        await skillRepo.save(
-          skillRepo.create({
-            title: `${user.name} skill.`,
-            description: `${user.name} skill is ${user.name}'s best skill!`,
-            images: ['images.jpg'],
-            owner: user,
-            category: someCategory,
-          }),
-        );
-      }
-      testUsers = await userRepo.findBy({
-        role: Role.USER,
-        wantToLearn: someCategory,
-      });
-
-      console.log('Тестовая БД успешна заполнена необходимыми значениями.');
-    } catch (error) {
-      console.error('Ошибка при подготовке к тестам:', error);
-    }
   });
 
   afterAll(async () => {
     await AppDataSource.destroy();
     await app.close();
+  });
+
+  it('Repositories should be seeded. Populating necessary data', async () => {
+    const testAllSkills = await skillRepo.find();
+    expect(testAllSkills.length).toBeGreaterThanOrEqual(1);
+    const testAllUsers = await userRepo.find();
+    expect(testAllUsers.length).toBeGreaterThanOrEqual(1);
+    const testAllCategories = await categoryRepo.find();
+    expect(testAllCategories.length).toBeGreaterThanOrEqual(1);
+
+    const existingSkills = await skillRepo.find({ relations: ['category'] });
+    const someExistingSkill = existingSkills[0];
+    someCategoryID = someExistingSkill.category.id;
+
+    // Creating some user for tests
+    someUser = {
+      name: 'Tests Users',
+      email: 'testsUsers@usersExample.com',
+      role: Role.USER,
+    };
+    userPassword = TestUserPassword;
+    someEmail = someUser.email;
+    await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({
+        email: someUser.email,
+        password: userPassword,
+        name: someUser.name,
+        gender: 'male',
+        avatar: 'default-avatar.png',
+      })
+      .expect(201);
+    someUserEntity = await userRepo.findOneOrFail({
+      where: {
+        email: someEmail,
+      },
+    });
+    someID = someUserEntity.id;
+
+    const authResponse: AuthResponse = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email: someEmail, password: userPassword });
+    const authUser = authResponse.body.user;
+    expect(authUser).toEqual(expect.objectContaining(someUser));
+    jwtToken = authResponse.body.tokens.accessToken;
+
+    // creating some skills for tests
+    const skillResponse: SkillResponse = await request(app.getHttpServer())
+      .post(`/skills`)
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send({
+        title: 'New Skill',
+        description: 'New Skill Description',
+        categoryId: someCategoryID,
+        images: ['shouldNotBeHere.jpg'],
+      })
+      .expect(201);
+    expect(skillResponse.body).toEqual(
+      expect.objectContaining({
+        title: 'New Skill',
+        description: 'New Skill Description',
+      }),
+    );
+    someSkillID = skillResponse.body.id;
+    someSkillEntity = await skillRepo.findOneOrFail({
+      where: {
+        id: someSkillID,
+      },
+    });
   });
 
   it('GET /users/ should return a list of users.', async () => {
@@ -218,27 +185,6 @@ describe('User module (e2e)', () => {
       .expect(200);
     expect(response.body.page).toBeGreaterThanOrEqual(1);
     expect(response.body.totalPages).toBeGreaterThanOrEqual(1);
-  });
-
-  it('GET /users/ populates data for following tests.', async () => {
-    const response: FindAllUsersResponse = await request(app.getHttpServer())
-      .get('/users/')
-      .expect(200);
-    expect(response.body.data).toEqual(
-      expect.arrayContaining([expect.objectContaining(TestUsersData[0])]),
-    );
-    let i = 0;
-    if (response.body.data[i].role === Role.ADMIN) {
-      i++;
-    }
-    someID = response.body.data[i].id;
-    someName = response.body.data[i].name;
-    someEmail = response.body.data[i].email;
-    someUser = { name: someName, email: someEmail, role: Role.USER };
-
-    expect(TestUsersData).toEqual(
-      expect.arrayContaining([expect.objectContaining(someUser)]),
-    );
   });
 
   it('GET /users/:id should return a user.', async () => {
@@ -268,13 +214,6 @@ describe('User module (e2e)', () => {
   });
 
   it('GET /users/me should return the current user.', async () => {
-    const authResponse: AuthResponse = await request(app.getHttpServer())
-      .post('/auth/login')
-      .send({ email: someEmail, password: userPassword });
-    const authUser = authResponse.body.user;
-    expect(authUser).toEqual(expect.objectContaining(someUser));
-    jwtToken = authResponse.body.tokens.accessToken;
-
     const response = await request(app.getHttpServer())
       .get(`/users/me`)
       .set('Authorization', `Bearer ${jwtToken}`)
@@ -297,7 +236,10 @@ describe('User module (e2e)', () => {
     const response = await request(app.getHttpServer())
       .patch(`/users/me`)
       .set('Authorization', `Bearer ${jwtToken}`)
-      .send({ about: 'Testing patching about data for this user' })
+      .send({
+        about: 'Testing patching about data for this user',
+        wantToLearn: [someCategoryID],
+      })
       .expect(200);
     expect(response.body).toEqual(
       expect.objectContaining({
@@ -386,23 +328,6 @@ describe('User module (e2e)', () => {
   });
 
   it('GET /users/by-skill/:id should return a user.', async () => {
-    const skillResponse: SkillResponse = await request(app.getHttpServer())
-      .post(`/skills`)
-      .set('Authorization', `Bearer ${jwtToken}`)
-      .send({
-        title: 'New Skill',
-        description: 'New Skill Description',
-        categoryId: someCategoryID,
-        images: ['shouldNotBeHere.jpg'],
-      })
-      .expect(201);
-    expect(skillResponse.body).toEqual(
-      expect.objectContaining({
-        title: 'New Skill',
-        description: 'New Skill Description',
-      }),
-    );
-    someSkillID = skillResponse.body.id;
     const response = await request(app.getHttpServer())
       .get(`/users/by-skill/${someSkillID}`)
       .expect(200);
@@ -415,15 +340,16 @@ describe('User module (e2e)', () => {
     )
       .get(`/users/similar-skill/${someSkillID}`)
       .expect(200);
-    testUsers.forEach((user) => {
-      if (user.name != someUser.name) {
-        expect(similarSkillResponse.body).toEqual(
-          expect.arrayContaining([
-            expect.objectContaining({ name: user.name, email: user.email }),
-          ]),
-        );
-      }
-    });
+    expect(similarSkillResponse.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ name: someUser.name, email: someUser.email }),
+      ]),
+    );
+  });
+
+  it('Post test cleanup', async () => {
+    await userRepo.remove(someUserEntity);
+    await skillRepo.remove(someSkillEntity);
   });
 
   afterAll(async () => {
